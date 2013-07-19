1e7273ed1e8ff5e96f646cc0ceb6d1eb6dcb4ae2
dstrek <batman@dstrek.com>
2013-07-20 01:05:04 +1000

    a grunt base to start this shit

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      dist: {
+        src: 'dist/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        options: {
+          jshintrc: 'lib/.jshintrc'
+        },
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'browserify', 'concat']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      }
+    },
+		browserify: {
+      client: {
+        src: ['lib/entry.js'],
+        dest: 'dist/app.js'
+      }
+    },
+		concat: {
+			dist: {
+        src: [
+					'vendor/Box2dWeb-2.1.a.3.js', 
+					'vendor/iioEngine.js', 
+					'vendor/iioDebugger.js',
+					'dist/app.js'
+				],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browserify');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'browserify', 'concat:dist', 'uglify']);
+
+};
